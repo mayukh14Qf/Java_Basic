563cbbd227570eb01d9409e4c955a9d4c11748e8
ABC <mayukc55@gmail.com>
2025-04-24 20:45:22 +0530

    Day 6

--- Day6/IMPOperatorConcept.java
+++ Day6/IMPOperatorConcept.java
@@ -3,12 +3,12 @@ package Day6;
 public class IMPOperatorConcept {
     public static void main(String[] args) {
 
-        // If I am Doing with 10 (Integer )
+        // If I am Doing with 10 (Integer)
 
         int var=1234;
 
-        System.out.println(var/10); // all the digits except last digit
-        System.out.println(var%10); // only the last digit
+        System.out.println(var/10); // all the digits except last digit (123)
+        System.out.println(var%10); // only the last digit (4)
 
         // If we have a float or double 10 (10.0) then it will normally divide
         System.out.println(var/10.0); // 123.4
--- Day6/typeCasting/Narrowing.java
+++ Day6/typeCasting/Narrowing.java
@@ -3,7 +3,7 @@ package Day6.typeCasting;
 public class Narrowing {
     public static void main(String[] args) {
 
-        // It will be done by the coder manually
+        // It will be done by the coder manually -----> Explicit Conversion
         // Storing the larger data type value to the smaller datatype variable
                 /*
 
@@ -12,19 +12,19 @@ public class Narrowing {
                  */
 
         int var1=(int)45.7f;
-        int var2=(byte)56.5;
+        int var2=(byte)56.5;  // decimal ----- > float / double
 
         System.out.println(var1);
         System.out.println(var2);
 
 
         int a=10;
-        System.out.println(a);
-        System.out.println((double) a);
+        System.out.println(a); // 10
+        System.out.println((double) a); // 10.0
 
 
         char var3=(char)65;
-        System.out.println(var3);
+        System.out.println(var3); // 65 -----> ascii character ----> 'A'
 
 
     }
